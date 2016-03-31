5de37f03f3d223b00424f741f781d439f98f4b67
Ricardo Klein <klein.rfk@gmail.com>
2016-03-31 15:18:00 -0300

    Changed MySQL DatabaseLLD script to better grep -v databases that does not matter

--- Template-Linux-App-MySQL/scripts/mysql_database_lld.sh
+++ Template-Linux-App-MySQL/scripts/mysql_database_lld.sh
@@ -3,7 +3,7 @@
 database_lld () {
         COUNTER=1
         echo -ne "{\n    \"data\":[\n"
-        DATABASES=$(HOME=/etc/zabbix mysql --batch --skip-column-names -e "show databases;" | grep -v "mysql" | grep -v "information_schema" | grep -v "test" | grep -v "performance_schema")
+        DATABASES=$(HOME=/etc/zabbix mysql --batch --skip-column-names -e "show databases;" | grep -v "^mysql$" | grep -v "^information_schema$" | grep -v "^test$" | grep -v "^performance_schema$")
         for DATABASE in $DATABASES
         do
                 if [ $COUNTER -ne 1 ]
